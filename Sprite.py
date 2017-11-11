3f3f802236926419064090c58a735e887e7c98c8
AlliterativeAnchovies <alliterative.anchovies@gmail.com>
2017-11-11 12:50:44 +0000

    Started adding Sprites
    I hate tabs/spaces/bananas

--- Sprite.py
+++ Sprite.py
@@ -1,1 +1,10 @@
 #This File Contains The Sprite Class
+
+class Sprite:
+    def __init__(self,spriteNames,locationX,locationY):
+        self.spriteDict = spriteNames
+        self.px = locationX
+        self.py = locationY
+        self.spriteOn = 0
+    def draw(self):
+        pygame.draw.rect(screen, (0,0,0), (100, 100, 100, 100))
--- actualpythontest.py
+++ actualpythontest.py
@@ -1,22 +1,24 @@
 import pygame
- 
+from Sprite import Sprite
+
 pygame.init()
 size = [640, 480]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('My Game')
 clock = pygame.time.Clock()
- 
+
 done = False
 while done == False:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
 
-	screen.fill((255, 255, 255)) 
+	screen.fill((255, 255, 255))
 
 	pygame.draw.rect(screen, (0,0,0), (100, 100, 100, 100))
 
+	spriteList = [Sprite('',0,0),Sprite('',1,1)]
 	pygame.display.update()
 	clock.tick(60)
- 
+
 pygame.quit()
