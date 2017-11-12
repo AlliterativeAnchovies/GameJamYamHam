3933f685ec07be39aae298a792195601c678b293
AlliterativeAnchovies <33570983+AlliterativeAnchovies@users.noreply.github.com>
2017-11-12 09:19:52 +0000

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,5 @@
+#This is our actual 'main' file that we should compile.  "actualpythontest.py" should
+#be used for testing (so we don't cause clashes if we both edited Main.py to test
+#our code.  It's okay if actualpythontest.py classes because it's not going to be part
+#of our actual finished project.  In fact, it should probably be added to a gitignore
+#but I'm too lazy to do that.
