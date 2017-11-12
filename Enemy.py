be408a34515ccbee522d54cbd81208c01e23c03d
Project <unconfigured@null.spigotmc.org>
2017-11-12 22:31:46 +0000

    Fixed the remaining conversions

--- Enemy.py
+++ Enemy.py
@@ -7,24 +7,24 @@ class Enemy(Movable):
   #takes in the arguments that create movable, as well as an id that
   #will define the enemy. (enemy's other stats loaded in from text file)
   #this should NOT be called on the outside
-  def __init__(self,sprites,locationX,locationY,id,behavior):
-	Movable.__init__(self,sprites,locationX,locationY)
-	self.id = id
-	self.behavior = behavior
-  #this should be called to create a new enemy
-  def create(id):
-	global spriteList
-	archetype = enemyArchetypes[id]
-	newEnemy = Enemy.clone(archetype)
-	spriteList.append(newEnemy)
-	enemyList.append(newEnemy)
-	return newEnemy
+	def __init__(self,sprites,locationX,locationY,id,behavior):
+		Movable.__init__(self,sprites,locationX,locationY)
+		self.id = id
+		self.behavior = behavior
+#this should be called to create a new enemy
+	def create(id):
+		global spriteList
+		archetype = enemyArchetypes[id]
+		newEnemy = Enemy.clone(archetype)
+		spriteList.append(newEnemy)
+		enemyList.append(newEnemy)
+		return newEnemy
 
-  def clone(self):
-	toReturn = Movable.clone(self)
-	toReturn.id = self.id
-	toReturn.behavior = self.behavior
-	return toReturn
+	def clone(self):
+		toReturn = Movable.clone(self)
+		toReturn.id = self.id
+		toReturn.behavior = self.behavior
+		return toReturn
 
 def initializeEnemies():
 	global enemyArchetypes
