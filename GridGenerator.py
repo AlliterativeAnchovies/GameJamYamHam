b590f90ba35821656ed3e97b781dc772b9a45d24
AlliterativeAnchovies <alliterative.anchovies@gmail.com>
2017-11-12 13:40:06 +0000

    sdf

--- GridGenerator.py
+++ GridGenerator.py
@@ -3,7 +3,7 @@ from Renderer import screen,size,clock,SPRITE_SIZE,GRID_SIZE,spriteList,gridList
 from Sprite import Sprite
 
 def generateNewGrid(left, top):
-    global board
+    global board, gridList
     fittingGrid = gridList[0]
     board.addToScreen(fittingGrid)
     return fittingGrid
