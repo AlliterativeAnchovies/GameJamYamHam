60a3610a7b28b0b4019258f962e4956c7fbbccdf
AlliterativeAnchovies <alliterative.anchovies@gmail.com>
2017-11-12 10:25:30 +0000

    A bit of formatting

--- Renderer.py
+++ Renderer.py
@@ -8,8 +8,9 @@ os.environ['SDL_VIDEO_CENTERED'] = '0'
 SPRITE_SIZE = 16
 GRID_SIZE = 8
 pygame.init()
-screenwidth, screenheight = pygame.display.Info().current_w, pygame.display.Info().current_h
-size = [screenwidth, SPRITE_SIZE*GRID_SIZE*6]
+SCREEN_WIDTH, RAW_SCREEN_HEIGHT = pygame.display.Info().current_w, pygame.display.Info().current_h
+SCREEN_HEIGHT = SPRITE_SIZE*GRID_SIZE*6
+size = [SCREEN_WIDTH, SCREEN_HEIGHT]
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 allSprites = {"test":pygame.image.load('resources/images/Test.bmp').convert()}
